3dee1f3a884bada9cf7025aaab6929700ccd69fa
Saad Srabon <saadsrabon2@gmail.com>
2025-05-19 00:46:38 +0600

    All Section formatting Done

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 
 import './App.css'
 import AboutSection from './components/shared/About'
+import { ContactForm, RequestServicesForm } from './components/shared/Contact'
 import HeroSection from './components/shared/Hero'
 import Header from './components/shared/Navbar'
 import ChefWebsite from './components/shared/Other'
@@ -14,6 +15,8 @@ function App() {
       <HeroSection/>
       <AboutSection/>
       <ChefWebsite/>
+      <ContactForm/>
+      <RequestServicesForm/>
     </>
   )
 }
