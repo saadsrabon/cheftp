167abd9382b79220a471d4dffa3a3c4d5a93e302
Saad Srabon <saadsrabon2@gmail.com>
2025-05-23 06:35:57 +0600

    fixed height

--- src/components/shared/Hero.jsx
+++ src/components/shared/Hero.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import travis from "../../assets/Travis Pena 1.png";
-
+import bg from "../../assets/Group 1000011749.png";
 const Hero = () => {
   return (
-    <section className="bg-[#C51212] text-white min-h-screen flex items-center justify-center relative px-6 pt-12">
+    <section className="bg-[#C51212] text-white h-[calc(100vh-190px)] flex items-center justify-center relative px-6 pt-12">
       <div className="absolute top-10 left-10 text-black max-w-[180px]">
         <p className="text-md leading-snug">Making people<br />happy by way of<br />food</p>
       </div>
@@ -15,11 +15,9 @@ const Hero = () => {
         <p className="text-sm">French<br />Italian</p>
       </div>
 
-      <div className="flex flex-col md:flex-row items-center gap-10">
+      <div className="flex flex-col md:flex-row items-center gap-x-20 py-20 absolute -bottom-23.5 ">
         <div className="text-black">
-          <p className="text-lg">Travelling Chef</p>
-          <h1 className="text-[80px] leading-none font-bold">Travis</h1>
-          <h1 className="text-[80px] leading-none font-bold">Pena</h1>
+          <img src={bg} alt="" />
         </div>
 
         <div className="relative">
